d70078c7493e9b040094d53fbaf577b9a22c9f47
Shiru-kage <epic7styleV2@gmail.com>
2025-06-16 19:08:45 +0800

    update game

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void ChangeScene(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeScene(int sceneIndex)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
