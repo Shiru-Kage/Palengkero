75c13ee391b6be630f4bcdf0271a33eca1e9ea2a
Shiru-kage <epic7styleV2@gmail.com>
2025-08-29 17:32:29 +0800

    Fix cursor manager

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -1,13 +1,72 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] private Texture2D cursorTexture;
-    private Vector2 cursorHotspot;
+    [Header("Cursor Textures")]
+    [SerializeField] private Texture2D defaultCursor;
+    [SerializeField] private Texture2D uiCursor;
 
-    void Start()
+    private Vector2 defaultHotspot;
+    private Vector2 uiHotspot;
+    private Texture2D currentCursor;
+
+    void Update()
     {
-        cursorHotspot = new Vector2 (cursorTexture.width / 2, cursorTexture.height / 2);
-        Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+        Texture2D newCursor = defaultCursor;
+        Vector2 hotspot = defaultHotspot;
+
+        if (EventSystem.current != null)
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition
+            };
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            foreach (var hit in results)
+            {
+                GameObject obj = hit.gameObject;
+
+                if (IsInteractiveUI(obj))
+                {
+                    newCursor = uiCursor;
+                    hotspot = uiHotspot;
+                    break;
+                }
+
+                var graphic = obj.GetComponent<Graphic>();
+                if (graphic != null && graphic.raycastTarget)
+                {
+                    break;
+                }
+            }
+        }
+
+        if (newCursor != currentCursor)
+        {
+            SetCursor(newCursor, hotspot);
+        }
+    }
+
+    private bool IsInteractiveUI(GameObject obj)
+    {
+        return obj.GetComponent<Button>() != null ||
+            obj.GetComponent<InputField>() != null ||
+            obj.GetComponent<TMP_InputField>() != null ||
+            obj.GetComponentInParent<Button>() != null ||
+            obj.GetComponentInParent<InputField>() != null ||
+            obj.GetComponentInParent<TMP_InputField>() != null;
+    }
+
+    private void SetCursor(Texture2D texture, Vector2 hotspot)
+    {
+        Cursor.SetCursor(texture, hotspot, CursorMode.Auto);
+        currentCursor = texture;
     }
 }
